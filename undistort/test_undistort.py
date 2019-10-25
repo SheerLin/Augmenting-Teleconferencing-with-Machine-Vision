0722103cf7636a00091e1231c0f7b070f9128088
SheerLin <sheer.linnan@outlook.com>
2019-10-25 11:09:44 -0700

    Update undistortion test case.

--- undistort/test_undistort.py
+++ undistort/test_undistort.py
@@ -5,26 +5,34 @@ original_chessboard_path = 'data/chessboard/original/*'
 profile_path = "profiles/test1.txt"
 img_path = "data/distorted/original/2wb1.jpg"
 
-undistort_instance = undistortion.Undistortion(profile_path=None,
-                                               chessboard_folder_path=original_chessboard_path)
-
-# undistort_instance.set_profile_path(profile_path)
-# undistort_instance.initialize()
-
-# img = cv2.imread(img_path)
-# cv2.imshow('img', img)
-# cv2.waitKey(400)
-
-# new_img = undistort_instance.calibrate_image(img)
-
-# cv2.imshow('img', new_img)
-# cv2.waitKey(500)
-
-original_img_points, original_obj_points = undistortion.Undistortion.serialize(undistort_instance.img_points,
-                                                                               undistort_instance.obj_points,
-                                                                               profile_path)
-
-new_img_points, new_obj_points = undistortion.Undistortion.deserialize(profile_path)
-
+# Test 1. Use chessboard folder
+# undistort_instance = undistortion.Undistortion(profile_path=None,
+#                                                chessboard_folder_path=original_chessboard_path)
+#
+# original_img_points, original_obj_points = undistortion.Undistortion.serialize(undistort_instance.img_points,
+#                                                                                undistort_instance.obj_points,
+#                                                                                profile_path)
+# new_img_points, new_obj_points = undistortion.Undistortion.deserialize(profile_path)
+#
 # undistortion.Undistortion.check_np_array_equals(original_img_points, new_img_points)
 # undistortion.Undistortion.check_np_array_equals(original_obj_points, new_obj_points)
+
+
+# Test 2. Use profile
+undistort_instance = undistortion.Undistortion(profile_path=profile_path,
+                                               chessboard_folder_path=None)
+
+
+#################################
+# Test single image calibration
+#################################
+img = cv2.imread(img_path)
+
+cv2.imshow('img', img)
+cv2.waitKey(400)
+
+new_img = undistort_instance.calibrate_image(img)
+
+cv2.imshow('img', new_img)
+cv2.waitKey(500)
+
